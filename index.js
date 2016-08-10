a00b3b7631cb430443f1acd4acf66866d4e2a0e8
Nicholas Kennedy <nick.kennedy@rangle.io>
2016-08-09 21:52:08 -0400

    Compiled with babel

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-var testHelpers = require('src/test');
-var assertions = require('src/assertions');
-
-module.exports = {
-  test: testHelpers.test,
-  suite: testHelpers.suite,
-  assertions: assertions,
-}
+var testPureJs = require('lib/index.js');
+module.exports = testPureJS;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+var testHelpers = require('./test');
+var assertions = require('./assertions');
+
+module.exports = {
+  test: testHelpers.test,
+  suite: testHelpers.suite,
+  assertions: assertions,
+}

f655bb60d0cd423c303badcefc9dcb1e6377e75c
Nicholas Kennedy <nick.kennedy@rangle.io>
2016-08-09 22:46:10 -0400

    Added runTests

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const testHelpers = require('./test');
 const assertions = require('./assertions');
 
 module.exports = {
+  runTests: testHelpers.runTests,
   test: testHelpers.test,
   suite: testHelpers.suite,
   assertions: assertions,
